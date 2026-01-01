8edd3f850a553f930db11ba18ca612f9c597464a
Andrey Salnikov <andrycalnicow@gmail.com>
2026-01-01 22:52:07 +0400

    update links on main branch

--- dist/RedMarble.user.js
+++ dist/RedMarble.user.js
@@ -1,20 +1,20 @@
 // ==UserScript==
 // @name         Red Marble
 // @namespace    https://github.com/SHshzik/
-// @version      0.86.7
+// @version      0.86.8
 // @description  A userscript to automate and/or enhance the user experience on Wplace.live. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with Wplace.live in any way, use at your own risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under the MPL-2.0 license. The "Blue Marble" icon is licensed under CC0 1.0 Universal (CC0 1.0) Public Domain Dedication. The image is owned by NASA.
 // @author       SwingTheVine
 // @license      MPL-2.0
 // @homepageURL  https://github.com/SHshzik/Wplace-BlueMarble-Dithering
-// @icon         https://raw.githubusercontent.com/SHshzik/Wplace-BlueMarble-Dithering/refs/heads/highlight/dist/assets/Favicon.png
-// @updateURL    https://github.com/SHshzik/Wplace-BlueMarble-Dithering/blob/highlight/dist/RedMarble.user.js
-// @downloadURL  https://github.com/SHshzik/Wplace-BlueMarble-Dithering/blob/highlight/dist/RedMarble.user.js
+// @icon         https://raw.githubusercontent.com/SHshzik/Wplace-BlueMarble-Dithering/refs/heads/main/dist/assets/Favicon.png
+// @updateURL    https://github.com/SHshzik/Wplace-BlueMarble-Dithering/blob/main/dist/RedMarble.user.js
+// @downloadURL  https://github.com/SHshzik/Wplace-BlueMarble-Dithering/blob/main/dist/RedMarble.user.js
 // @match        https://wplace.live/*
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // @grant        GM.setValue
 // @grant        GM_getValue
-// @resource     CSS-BM-File https://raw.githubusercontent.com/SHshzik/Wplace-BlueMarble-Dithering/refs/heads/highlight/dist/RedMarble.user.css
+// @resource     CSS-BM-File https://raw.githubusercontent.com/SHshzik/Wplace-BlueMarble-Dithering/refs/heads/main/dist/RedMarble.user.css
 // ==/UserScript==
 
 // Wplace  --> https://wplace.live
